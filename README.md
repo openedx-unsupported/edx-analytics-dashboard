6f52f6c6eac31528afd41ab1848d570e66f6373b
Clinton Blackburn <cblackburn@edx.org>
2014-06-18 13:50:15 -0400

    Added Basic Django Project
    Used the django-twoscoops-project template.

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ analytics-dashboard
 ===================
 
 Dashboard to display course analytics to course teams
+
+Getting Started
+---------------
+
+1. Get the code (e.g. clone the repository).
+2. Install the requirements:
+
+        $ pip install -r requirements/local.txt
+
+3. Run the server:
+
+        ./manage.py runserver
+
+Testing
+-------
+
+    $ ./manage.py test
---
+++ analytics_dashboard/analytics_dashboard/settings/test.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+from .base import *
+
+########## TEST SETTINGS
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
+TEST_DISCOVER_ROOT = SITE_ROOT
+TEST_DISCOVER_PATTERN = "test_*.py"
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
