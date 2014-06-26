1419673e8aa17e70dd6eb26f1a54586fc1293bbd
Dennis Jen <djen@edx.org>
2014-06-26 17:37:20 -0400

    Added first model and view for the lens navigation, reworked tests to run jasmine via karma
      * added spec runner page for viewing and running test results
      * added karma configuration to run tests via phantomjs
      * added lens navigation view, which displays a basic message
      * data is inserted to the page template and passed to JS
      * added course model
    
    AN-1293 # resolve
    AN-1357 # resolve
    
    Change-Id: I0cf7a3fb9f7b82e0ff167c8bb8cd7ad7f39e4d8a

---
+++ karma.conf.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Thu Jun 26 2014 17:49:39 GMT-0400 (EDT)
+
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'requirejs'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            {pattern: 'analytics_dashboard/static/vendor/**/*.js', included: false},
+            {pattern: 'analytics_dashboard/static/js/models/**/*.js', included: false},
+            {pattern: 'analytics_dashboard/static/js/test/specs/*.js', included: false},
+            'analytics_dashboard/static/js/test/spec-runner.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [
+
+        ],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        // you can also add Chrome or other browsers too
+        browsers: ['PhantomJS'],
+
+        captureTimeout: 60000,
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
+};
