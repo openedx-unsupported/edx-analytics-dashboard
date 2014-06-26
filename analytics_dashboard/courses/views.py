133a880aacb32744d7b76119b0c7b3621e70b2fa
Dennis Jen <djen@edx.org>
2014-06-26 17:37:20 -0400

    Added first model and view for the lens navigation, reworked tests to run jasmine via karma
      * added spec runner page for viewing and running test results
      * added karma configuration to run tests via phantomjs
      * added lens navigation view, which displays a basic message
      * data is inserted to the page template and passed to JS
      * added course model
    
    AN-1293 # resolve
    AN-1357 # resolve
    
    Change-Id: I0cf7a3fb9f7b82e0ff167c8bb8cd7ad7f39e4d8a

--- analytics_dashboard/courses/views.py
+++ analytics_dashboard/courses/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+import json
 
 from models import Course
 
 def analytics(request, course_id):
+    page_data = {
+        'name': 'Dennis',
+        'courseId': str(course_id),
+    }
     # we would ideally get this from the DB, but for now lets stub some data
     context = {'user_name': 'Ed Xavier',
                'course_number': 'MITx 7.3423',
-               'course_title': 'Introduction to Awesomeness'
+               'course_title': 'Introduction to Awesomeness',
+               'page_data': json.dumps(page_data)
     }
     return render(request, 'courses/analytics.html', context)
--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,49 @@
 
     var jshint = require('gulp-jshint');
     var gulp = require('gulp');
-    var jasmine = require('gulp-jasmine');
+    var karma = require('karma').server;
+    var path = require('path');
+
     // lint this file in addition to js files.  Please add directories to this
     // as needed
-    var lintSources = [
+    var paths = {
+        // add your test directories here
+        spec: ['analytics_dashboard/static/js/spec/specs/course-model-spec.js'],
+        //spec: ['analytics_dashboard/static/js/spec/spec-runner.js'],
+        lint: [
             'gulpfile.js',
-            'analytics_dashboard/static/js/*.js',
-            'analytics_dashboard/static/js/spec/*.js'];
-    // add your test directories here
-    var testSources = ['analytics_dashboard/static/js/spec/test.js'];
+            'analytics_dashboard/static/js/**/.js',
+            'analytics_dashboard/static/js/test/**/.js'],
+        karamaConf: 'karma.conf.js'
+    };
+
+    // kicks up karma to the tests once
+    function runKarma(configFile, cb) {
+       karma.start({
+          configFile: path.resolve(configFile),
+          singleRun: true
+       }, cb);
+    }
 
     gulp.task('lint', function() {
-        return gulp.src(lintSources)
+        return gulp.src(paths.lint)
                 .pipe(jshint())
                 .pipe(jshint.reporter('default'));
     });
 
-    gulp.task('test', function () {
-        gulp.src(testSources)
-            .pipe(jasmine());
+    // this task runs the tests.  It doesn't give you very detailed results,
+    // so you may need to run the jasmine test page directly:
+    //      http://127.0.0.1:8000/static/js/test/spec-runner.html
+    gulp.task('test', function(cb) {
+       runKarma(paths.karamaConf, cb);
     });
 
     // these are the default tasks when you run gulp
     gulp.task('default', ['lint', 'test']);
 
-    // type 'gulp watch' to continuously run linting
+    // type 'gulp watch' to continuously run linting and tests
     gulp.task('watch', function() {
-        gulp.watch(lintSources, ['lint', 'test']);
+        gulp.watch(paths.spec, ['lint', 'test']);
     });
 
 }());
