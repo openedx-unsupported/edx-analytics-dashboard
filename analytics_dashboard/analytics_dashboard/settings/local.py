9616e473c4246b88ad9ea57c03fad17c6881a6b5
Dennis Jen <djen@edx.org>
2014-07-03 14:26:42 -0400

    Added bok choy acceptance test framework
      * added one simple acceptance test
      * added configuration to the settings for the acceptance test server
    
    AN-1393 # resolve
    
    Change-Id: I98b6b2f05387e7b382eef29381b84b0ff48d5dce

--- analytics_dashboard/analytics_dashboard/settings/local.py
+++ analytics_dashboard/analytics_dashboard/settings/local.py
@@ -62,3 +62,10 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+
+########## ACCEPTANCE TEST CONFIGURATION
+ACCEPTANCE_TEST_SERVER = {
+    'address': '127.0.0.1',
+    'port': 8000
+}
+
