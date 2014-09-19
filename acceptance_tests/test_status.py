834c58157608faac305d00ab0201ee77dd13e65a
Clinton Blackburn <cblackburn@edx.org>
2014-09-19 15:41:55 -0400

    Added Acceptance Tests for Health and Status Paths

---
+++ acceptance_tests/test_status.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+import requests
+from acceptance_tests import DASHBOARD_SERVER_URL
+
+
+class StatusTests(TestCase):
+    POSITIVE_STATUS = u'OK'
+
+    def test_health(self):
+        response = requests.get('{}/health'.format(DASHBOARD_SERVER_URL))
+
+        self.assertEqual(response.status_code, 200)
+
+        expected_status = {
+            'overall_status': self.POSITIVE_STATUS,
+            'detailed_status': {
+                'database_connection': self.POSITIVE_STATUS,
+                'analytics_api': self.POSITIVE_STATUS
+            }
+        }
+        self.assertDictEqual(response.json(), expected_status)
+
+    def test_status(self):
+        response = requests.get('{}/status'.format(DASHBOARD_SERVER_URL))
+
+        self.assertEqual(response.status_code, 200)
