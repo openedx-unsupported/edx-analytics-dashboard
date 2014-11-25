8210930daf7084a8562a60be28b3543dbedbf58a
Dennis Jen <djen@edx.org>
2014-11-25 09:49:07 -0500

    Updated no data message to remove example text.

--- analytics_dashboard/analytics_dashboard/settings/production.py
+++ analytics_dashboard/analytics_dashboard/settings/production.py
@@ -62,6 +62,5 @@ for override, value in DB_OVERRIDES.iteritems():
 FULL_APPLICATION_NAME = '{0} {1}'.format(PLATFORM_NAME, APPLICATION_NAME)
 
 # Depends on DOCUMENTATION_LOAD_ERROR_URL, so evaluate at the end
-DOCUMENTATION_LOAD_ERROR_MESSAGE = 'This data may not be available for your course. For example, student ' \
-                                   'enrollment data is not computed for courses created prior to December 4, 2013. ' \
+DOCUMENTATION_LOAD_ERROR_MESSAGE = 'This data may not be available for your course. ' \
                                    '<a href="{error_documentation_link}" target="_blank">Read more</a>.'.format(error_documentation_link=DOCUMENTATION_LOAD_ERROR_URL)
