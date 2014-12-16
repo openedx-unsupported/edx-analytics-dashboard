37f819f99b48d5db1a38358c5a77d0ce05cfd729
Clinton Blackburn <cblackburn@edx.org>
2014-12-16 16:23:50 -0500

    Cleaned URL Patterns
    Using include to prepare course-specific URLs instead of a for-loop.

--- analytics_dashboard/courses/urls.py
+++ analytics_dashboard/courses/urls.py
@@ -1,40 +1,39 @@
 # pylint: disable=no-value-for-parameter
 
-from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns, include
 
 from courses import views
 
-COURSE_URLS = [
-    ('enrollment/activity', views.EnrollmentActivityView.as_view()),
-    ('enrollment/geography', views.EnrollmentGeographyView.as_view()),
-    ('enrollment/demographics/age', views.EnrollmentDemographicsAgeView.as_view()),
-    ('enrollment/demographics/education', views.EnrollmentDemographicsEducationView.as_view()),
-    ('enrollment/demographics/gender', views.EnrollmentDemographicsGenderView.as_view()),
-    ('engagement/content', views.EngagementContentView.as_view()),
-    ('csv/enrollment', views.CourseEnrollmentCSV.as_view()),
-    ('csv/enrollment_by_country', views.CourseEnrollmentByCountryCSV.as_view()),
-    ('csv/enrollment_demographics_age', views.CourseEnrollmentDemographicsAgeCSV.as_view()),
-    ('csv/enrollment_demographics_education', views.CourseEnrollmentDemographicsEducationCSV.as_view()),
-    ('csv/enrollment_demographics_gender', views.CourseEnrollmentDemographicsGenderCSV.as_view()),
-    ('csv/engagement_activity_trend', views.CourseEngagementActivityTrendCSV.as_view()),
-]
-
 COURSE_ID_PATTERN = r'(?P<course_id>[^/+]+[/+][^/+]+[/+][^/]+)'
+
+COURSE_URLS = patterns(
+    '',
+    # Course homepage. This should be the entry point for other applications linking to the course.
+    url(r'^$', views.CourseHome.as_view(), name='home'),
+    url(r'^enrollment/activity/$', views.EnrollmentActivityView.as_view(), name='enrollment_activity'),
+    url(r'^enrollment/geography/$', views.EnrollmentGeographyView.as_view(), name='enrollment_geography'),
+    url(r'^enrollment/demographics/age/$', views.EnrollmentDemographicsAgeView.as_view(),
+        name='enrollment_demographics_age'),
+    url(r'^enrollment/demographics/education/$', views.EnrollmentDemographicsEducationView.as_view(),
+        name='enrollment_demographics_education'),
+    url(r'^enrollment/demographics/gender/$', views.EnrollmentDemographicsGenderView.as_view(),
+        name='enrollment_demographics_gender'),
+    url(r'^engagement/content/$', views.EngagementContentView.as_view(), name='engagement_content'),
+    url(r'^csv/enrollment/$', views.CourseEnrollmentCSV.as_view(), name='csv_enrollment'),
+    url(r'^csv/enrollment_by_country/$', views.CourseEnrollmentByCountryCSV.as_view(),
+        name='csv_enrollment_by_country'),
+    url(r'^csv/enrollment_demographics_age/$', views.CourseEnrollmentDemographicsAgeCSV.as_view(),
+        name='csv_enrollment_demographics_age'),
+    url(r'^csv/enrollment_demographics_education/$', views.CourseEnrollmentDemographicsEducationCSV.as_view(),
+        name='csv_enrollment_demographics_education'),
+    url(r'^csv/enrollment_demographics_gender/$', views.CourseEnrollmentDemographicsGenderCSV.as_view(),
+        name='csv_enrollment_demographics_gender'),
+    url(r'^csv/engagement_activity_trend/$', views.CourseEngagementActivityTrendCSV.as_view(),
+        name='csv_engagement_activity_trend')
+)
 
 urlpatterns = patterns(
     '',
     url('^$', views.CourseIndex.as_view(), name='index'),
-
-    # Course homepage. This should be the entry point for other applications linking to the course.
-    url(r'^{0}/$'.format(COURSE_ID_PATTERN), views.CourseHome.as_view(), name='home')
+    url(r'^{}/'.format(COURSE_ID_PATTERN), include(COURSE_URLS))
 )
-
-
-def generate_regex(path):
-    return r'^{0}/{1}/$'.format(COURSE_ID_PATTERN, path)
-
-
-for name, view in COURSE_URLS:
-    # Create a new URL pattern. Slashes are valid for paths, but not names. Replace any forward slashes
-    # with an underscore.
-    urlpatterns += patterns('', url(generate_regex(name), view, name=name.replace('/', '_')))
