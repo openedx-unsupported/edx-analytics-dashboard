ee988b8b61702bceafe5d5c3a6a6c1e2717d5f2e
Ben Patterson <bpatterson@edx.org>
2015-02-06 16:48:30 -0500

    Send only unit and JS data to datadog.
    Acceptance test coverage data can be sorted out separately.

--- scripts/report-coverage-to-datadog.sh
+++ scripts/report-coverage-to-datadog.sh
@@ -7,30 +7,28 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n $DATADOG_API_KEY ]
 
+    then
     echo "Reporting coverage stats to datadog"
-    git clone test-metrics https://github.com/wedaly/test-metrics
+    git clone https://github.com/wedaly/test-metrics
 
     cd test-metrics
     virtualenv venv
     source ./venv/bin/activate
-    pip install -r requirements.txt
+    pip install -q -r requirements.txt
 
     cat > unit_test_groups.json <<END
-    {
-        "unit.analytics_dashboard": "analytics_dashboard/*.py",
-        "javascript.analytics_dashboard": "*.js"
-    }
-    END
-
-    cat > acceptance_test_group.json <<END
-    {
-        "acceptance.analytics_dashboard": "analytics_dashboard/*.py"
-    }
-    END
+{
+    "unit.analytics_dashboard": "analytics_dashboard/*.py",
+    "javascript.analytics_dashboard": "*.js"
+}
+END
 
     python -m metrics.coverage unit_test_groups.json `find ../build -name "coverage.xml"`
-    python -m metrics.coverage acceptance_test_group.json `find ../acceptance_tests/build -name "coverage.xml"`
 
     deactivate
+
+else
+    echo "Note: Not reporting stats to datadog. Those are only reported for builds on master, \
+and when the datadog api key is available."
 
 fi
