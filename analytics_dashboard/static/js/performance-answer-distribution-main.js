acaf417c03b8c86341047220275c60980b3f5634
Ben Patterson <bpatterson@edx.org>
2015-02-09 09:49:53 -0500

    Make compliant with jscs >= 1.10

--- analytics_dashboard/static/js/performance-answer-distribution-main.js
+++ analytics_dashboard/static/js/performance-answer-distribution-main.js
@@ -8,63 +8,63 @@ require(['vendor/domReady!', 'load/init-page'], function(doc, page) {
     require(['underscore', 'views/data-table-view', 'views/discrete-bar-view'],
             function (_, DataTableView, DiscreteBarView) {
 
-        var courseModel = page.models.courseModel,
-            answerField = 'answer_value',
-            answerColumn = {key: answerField, title: gettext('Answer'), type:'hasNull'},
-            tableColumns = [
-                answerColumn,
-                {key: 'correct', title: gettext('Correct'), type: 'bool'},
-                {key: 'count', title: gettext('Submission Count'), type: 'number', className: 'text-right'}
-            ];
+                var courseModel = page.models.courseModel,
+                    answerField = 'answer_value',
+                    answerColumn = {key: answerField, title: gettext('Answer'), type:'hasNull'},
+                    tableColumns = [
+                        answerColumn,
+                        {key: 'correct', title: gettext('Correct'), type: 'bool'},
+                        {key: 'count', title: gettext('Submission Count'), type: 'number', className: 'text-right'}
+                    ];
 
-        // answers are stored either the numeric or string fields
-        if (courseModel.get('answerType') === 'numeric') {
-            answerColumn.type = 'number';
-        }
-
-        // randomized problems have a random seeds field that differentiates rows
-        if (courseModel.get('isRandom')) {
-            // only show the variant column for randomized problems
-            tableColumns.push({
-                key: 'variant', title: gettext('Variant'), type: 'number', className: 'text-right'
-            });
-        }
-
-        new DiscreteBarView({
-            el: '#performance-chart-view',
-            model: courseModel,
-            modelAttribute: 'answerDistributionLimited',
-            dataType: 'count',
-            trends: [{
-                title: function(index) {
-                    if (courseModel.get('answerDistributionLimited')[index].correct) {
-                        return 'Correct';
-                    } else {
-                        return 'Incorrect';
-                    }
-                },
-                color: function (bar, index) {
-                    // green bars represent bars with the correct answer
-                    if (courseModel.get('answerDistributionLimited')[index].correct) {
-                        return 'rgb(0, 137, 0)';
-                    } else {
-                        return 'rgb(58, 162, 224)';
-                    }
+                // answers are stored either the numeric or string fields
+                if (courseModel.get('answerType') === 'numeric') {
+                    answerColumn.type = 'number';
                 }
-            }],
-            x: { key: answerField },
-            y: { key: 'count' },
-            // Translators: <%=value%> will be replaced by a student response to a question asked in a course.
-            interactiveTooltipHeaderTemplate: _.template(gettext('Answer: <%=value%>'))
-        });
 
-        new DataTableView({
-            el: '[data-role=performance-table]',
-            model: courseModel,
-            modelAttribute: 'answerDistribution',
-            columns: tableColumns,
-            sorting: ['-count']
-        });
+                // randomized problems have a random seeds field that differentiates rows
+                if (courseModel.get('isRandom')) {
+                    // only show the variant column for randomized problems
+                    tableColumns.push({
+                        key: 'variant', title: gettext('Variant'), type: 'number', className: 'text-right'
+                    });
+                }
 
-    });
+                new DiscreteBarView({
+                    el: '#performance-chart-view',
+                    model: courseModel,
+                    modelAttribute: 'answerDistributionLimited',
+                    dataType: 'count',
+                    trends: [{
+                        title: function(index) {
+                            if (courseModel.get('answerDistributionLimited')[index].correct) {
+                                return 'Correct';
+                            } else {
+                                return 'Incorrect';
+                            }
+                        },
+                        color: function (bar, index) {
+                            // green bars represent bars with the correct answer
+                            if (courseModel.get('answerDistributionLimited')[index].correct) {
+                                return 'rgb(0, 137, 0)';
+                            } else {
+                                return 'rgb(58, 162, 224)';
+                            }
+                        }
+                    }],
+                    x: { key: answerField },
+                    y: { key: 'count' },
+                    // Translators: <%=value%> will be replaced by a student response to a question asked in a course.
+                    interactiveTooltipHeaderTemplate: _.template(gettext('Answer: <%=value%>'))
+                });
+
+                new DataTableView({
+                    el: '[data-role=performance-table]',
+                    model: courseModel,
+                    modelAttribute: 'answerDistribution',
+                    columns: tableColumns,
+                    sorting: ['-count']
+                });
+
+            });
 });
