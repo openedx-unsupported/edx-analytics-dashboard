51dbde617c61be40d8edc6520ac2e2ddcd239424
Clinton Blackburn <clinton.blackburn@gmail.com>
2015-02-26 21:53:34 -0500

    JS Optimization
    Added data table view to main application file. This should result in the code being loaded once, upon the initial visit, and not being loaded for every data page. Total savings should be about 100KB (uncompressed) for each data page load.

--- analytics_dashboard/static/js/application-main.js
+++ analytics_dashboard/static/js/application-main.js
@@ -2,8 +2,12 @@
  * Load scripts needed across the application.
  */
 
-require(['bootstrap', 'bootstrap_accessibility', 'vendor/domReady!', 'load/init-page', 'views/announcement-view'],
-    function (bootstrap, bootstrap_accessibility, doc, page, AnnouncementView) {
+require(['bootstrap',
+        'bootstrap_accessibility',
+        'vendor/domReady!', 'load/init-page',
+        'views/data-table-view',
+        'views/announcement-view'],
+    function (bootstrap, bootstrap_accessibility, doc, page, DataTableView, AnnouncementView) {
         'use strict';
 
         // Instantiate the announcement view(s)
