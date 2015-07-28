812a172c19a756bd2e50260599437626a81b558e
Christine Lytwynec <clytwynec@edx.org>
2015-07-28 11:32:52 -0400

    Combine DashboardPage and CoursePage for a11y tests

--- a11y_tests/pages.py
+++ a11y_tests/pages.py
@@ -6,33 +6,27 @@ from bok_choy.page_object import PageObject
 from acceptance_tests import TEST_COURSE_ID, DASHBOARD_SERVER_URL
 
 
-class DashboardPage(PageObject):  # pylint: disable=abstract-method
-    path = None
+class CoursePage(PageObject):
     basic_auth_username = None
     basic_auth_password = None
 
-    @property
-    def url(self):
-        return self.page_url
-
-    def __init__(self, browser, path=None):
-        super(DashboardPage, self).__init__(browser)
-        path = path or self.path
-        self.server_url = DASHBOARD_SERVER_URL
-        self.page_url = '{0}/{1}'.format(self.server_url, path)
-
-
-class CoursePage(DashboardPage):
     def __init__(self, browser, course_id=None):
         # Create the path
         self.course_id = course_id or TEST_COURSE_ID
         path = 'courses/{}'.format(self.course_id)
 
+        self.server_url = DASHBOARD_SERVER_URL
+        self.page_url = '{0}/{1}'.format(self.server_url, path)
+
         # Call the constructor and setup the URL
-        super(CoursePage, self).__init__(browser, path)
+        super(CoursePage, self).__init__(browser)
 
     def is_browser_on_page(self):
         return self.browser.current_url == self.page_url
+
+    @property
+    def url(self):
+        return self.page_url
 
 
 class CourseEnrollmentDemographicsPage(CoursePage):
