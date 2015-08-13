138d4c7a0f4b480353a5c8462551c39f3bb9455d
Tim Krones <t.krones@gmx.net>
2015-08-13 17:27:25 +0200

    Add page that lists number of attempts and final score for all problems attempted by a given user.

--- analytics_dashboard/users/urls.py
+++ analytics_dashboard/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.UserListView.as_view(), name='list'),
     url(r'^(?P<username>[^/]+)/$', views.UserProfileView.as_view(), name='profile'),
+    url(r'^(?P<username>[^/]+)/problems/$', views.UserProblemDataView.as_view(), name='problems')
 )
---
+++ analytics_dashboard/users/views/problem_data.py
@@ -0,0 +1,59 @@
+from django.conf import settings
+
+from common.clients import CourseStructureApiClient
+from .base import UsersView
+
+
+class UserProblemDataView(UsersView):
+    template_name = 'users/problem_data.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        access_token = settings.COURSE_API_KEY or request.user.access_token
+        self.course_api_client = CourseStructureApiClient(settings.COURSE_API_URL, access_token)
+
+        return super(UserProblemDataView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProblemDataView, self).get_context_data(**kwargs)
+
+        course_structure = self.course_api_client.course_structures(self.course_id).get()
+        blocks = course_structure.get('blocks', {})
+
+        username = kwargs['username']
+        profile = self.client.users(username).profile()
+        user_id = profile['id']
+
+        weekly_problem_data = self.client.user_problem_weekly_data(self.course_id, user_id).weekly_problem_data()
+
+        # High-level steps for each entry in weekly_problem_data:
+        # - Calculate total number of attempts from weekly number of attempts
+        # - Set final score to final score of most recent week
+
+        problem_data = {}
+
+        for entry in weekly_problem_data:
+            week_ending = entry['week_ending']
+            problem_id = entry['problem_id']
+            num_attempts = entry['num_attempts']
+            final_score = '{}/{}'.format(entry['most_recent_score'], entry['max_score'])
+            problem_name = blocks[problem_id].get('display_name', problem_id)
+
+            if problem_id not in problem_data:
+                problem_data[problem_id] = {
+                    'week_ending': week_ending,
+                    'name': problem_name,
+                    'num_attempts': num_attempts,
+                    'final_score': final_score,
+                }
+            else:
+                problem = problem_data[problem_id]
+                problem['num_attempts'] += num_attempts
+                if week_ending > problem['week_ending']:
+                    problem['week_ending'] = week_ending
+                    problem['final_score'] = final_score
+                problem_data[problem_id] = problem
+
+        context['username'] = username
+        context['problem_data'] = problem_data.values()
+
+        return context
