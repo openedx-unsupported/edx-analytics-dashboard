c553da9b8d5b51ea47f9e156088d624acf0c1f50
Daniel Friedman <dfriedman58@gmail.com>
2015-12-21 17:09:50 -0500

    Add learner analytics UI scaffolding
    AN-6199

--- analytics_dashboard/courses/urls.py
+++ analytics_dashboard/courses/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls import url, patterns, include
 
 from courses import views
-from courses.views import enrollment, engagement, performance, csv
+from courses.views import enrollment, engagement, performance, csv, learners
 
 CONTENT_ID_PATTERN = r'(?P<content_id>(?:i4x://?[^/]+/[^/]+/[^/]+/[^@]+(?:@[^/]+)?)|(?:[^/]+))'
 PROBLEM_PART_ID_PATTERN = CONTENT_ID_PATTERN.replace('content_id', 'problem_part_id')
@@ -101,6 +101,11 @@ CSV_URLS = patterns(
         name='performance_answer_distribution'),
 )
 
+LEARNER_URLS = patterns(
+    '',
+    url(r'^$', learners.LearnersView.as_view(), name='learners'),
+)
+
 COURSE_URLS = patterns(
     '',
     # Course homepage. This should be the entry point for other applications linking to the course.
@@ -109,6 +114,7 @@ COURSE_URLS = patterns(
     url(r'^engagement/', include(ENGAGEMENT_URLS, namespace='engagement')),
     url(r'^performance/', include(PERFORMANCE_URLS, namespace='performance')),
     url(r'^csv/', include(CSV_URLS, namespace='csv')),
+    url(r'^learners/', include(LEARNER_URLS, namespace='learners')),
 )
 
 urlpatterns = patterns(
