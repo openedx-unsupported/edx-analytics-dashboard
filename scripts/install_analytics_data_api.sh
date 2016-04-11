af33832808fe8d7a5870b3427de69e53061759ed
Dennis Jen <djen@edx.org>
2016-04-11 16:32:32 -0400

    Use master branch of data api.

--- scripts/install_analytics_data_api.sh
+++ scripts/install_analytics_data_api.sh
@@ -2,7 +2,6 @@
 
 git clone https://github.com/edx/edx-analytics-data-api.git
 cd edx-analytics-data-api
-git checkout learner-analytics
 pip install -q -r requirements/base.txt
 make test.install_elasticsearch
 cd -
