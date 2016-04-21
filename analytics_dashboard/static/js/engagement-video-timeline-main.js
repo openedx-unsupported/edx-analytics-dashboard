86aa4ba369f8dfeee855995f8814cace7dc48623
Daniel Friedman <dfriedman58@gmail.com>
2016-04-21 11:31:37 -0400

    Update require config references to edx-ui-toolkit

--- analytics_dashboard/static/js/engagement-video-timeline-main.js
+++ analytics_dashboard/static/js/engagement-video-timeline-main.js
@@ -5,58 +5,68 @@
 require(['vendor/domReady!', 'load/init-page'], function(doc, page) {
     'use strict';
 
-    require(['disclosure', 'underscore', 'views/data-table-view', 'views/iframe-view', 'views/stacked-timeline-view'],
-        function (DisclosureView, _, DataTableView, IFrameView, StackedTimelineView) {
+    require([
+        'uitk/disclosure/disclosure-view',
+        'underscore',
+        'views/data-table-view',
+        'views/iframe-view',
+        'views/stacked-timeline-view'
+    ], function (
+        DisclosureView,
+        _,
+        DataTableView,
+        IFrameView,
+        StackedTimelineView
+    ) {
 
-            var courseModel = page.models.courseModel,
-                timelineSettings = [
-                    {
-                        key: 'num_users',
-                        title: gettext('Unique Viewers'),
-                        className: 'text-right',
-                        type: 'number',
-                        color: 'rgb(61,162,229)'
-                    },
-                    {
-                        key: 'num_replays',
-                        title: gettext('Replays'),
-                        className: 'text-right',
-                        type: 'number',
-                        color: 'rgb(18,46,204)'
-                    }
-                ],
-                tableColumns = [
-                    {key: 'start_time', title: gettext('Time'), type: 'time'}
-                ];
+        var courseModel = page.models.courseModel,
+            timelineSettings = [
+                {
+                    key: 'num_users',
+                    title: gettext('Unique Viewers'),
+                    className: 'text-right',
+                    type: 'number',
+                    color: 'rgb(61,162,229)'
+                },
+                {
+                    key: 'num_replays',
+                    title: gettext('Replays'),
+                    className: 'text-right',
+                    type: 'number',
+                    color: 'rgb(18,46,204)'
+                }
+            ],
+            tableColumns = [
+                {key: 'start_time', title: gettext('Time'), type: 'time'}
+            ];
 
-            tableColumns = tableColumns.concat(timelineSettings);
+        tableColumns = tableColumns.concat(timelineSettings);
 
-            new DisclosureView({
-                el: '.module-preview-disclosure'
-            });
+        new DisclosureView({
+            el: '.module-preview-disclosure'
+        });
 
-            // loading the iframe blocks content, so load it after the rest of the page loads
-            new IFrameView({
-                el: '#module-preview',
-                loadingSelector: '#module-loading'
-            });
+        // loading the iframe blocks content, so load it after the rest of the page loads
+        new IFrameView({
+            el: '#module-preview',
+            loadingSelector: '#module-loading'
+        });
 
-            new StackedTimelineView({
-                el: '#chart-view',
-                model: courseModel,
-                modelAttribute: 'videoTimeline',
-                trends: timelineSettings,
-                x: { key: 'start_time', title: 'Time' },
-                y: { key: 'num_users' }
-            });
+        new StackedTimelineView({
+            el: '#chart-view',
+            model: courseModel,
+            modelAttribute: 'videoTimeline',
+            trends: timelineSettings,
+            x: { key: 'start_time', title: 'Time' },
+            y: { key: 'num_users' }
+        });
 
-            new DataTableView({
-                el: '[data-role=data-table]',
-                model: courseModel,
-                modelAttribute: 'videoTimeline',
-                columns: tableColumns
-            });
+        new DataTableView({
+            el: '[data-role=data-table]',
+            model: courseModel,
+            modelAttribute: 'videoTimeline',
+            columns: tableColumns
+        });
 
-        }
-    );
+    });
 });
