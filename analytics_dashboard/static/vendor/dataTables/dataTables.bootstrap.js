e440d29b890ddd82e296128f9bba608ecba40fce
Anjali Pal <apal@edx.org>
2016-07-12 16:08:20 -0400

    * More autofixes * Add .eslintignore

--- analytics_dashboard/static/vendor/dataTables/dataTables.bootstrap.js
+++ analytics_dashboard/static/vendor/dataTables/dataTables.bootstrap.js
@@ -10,110 +10,109 @@
  * controls using Bootstrap. See http://datatables.net/manual/styling/bootstrap
  * for further information.
  */
-(function(window, document, undefined){
-
-var factory = function( $, DataTable ) {
-"use strict";
+(function(window, document, undefined) {
+    var factory = function($, DataTable) {
+        'use strict';
 
 
 /* Set the defaults for DataTables initialisation */
-$.extend( true, DataTable.defaults, {
-	dom:
-		"<'row'<'col-xs-6'l><'col-xs-6'f>r>"+
-		"t"+
+        $.extend(true, DataTable.defaults, {
+	                                                                                                            dom:
+		"<'row'<'col-xs-6'l><'col-xs-6'f>r>" +
+		't' +
 		"<'row'<'col-xs-6'i><'col-xs-6'p>>",
-	renderer: 'bootstrap'
-} );
+	                                                                                                            renderer: 'bootstrap'
+        });
 
 
 /* Default class modification */
-$.extend( DataTable.ext.classes, {
-	sWrapper:      "dataTables_wrapper form-inline dt-bootstrap",
-	sFilterInput:  "form-control input-sm",
-	sLengthSelect: "form-control input-sm"
-} );
+        $.extend(DataTable.ext.classes, {
+	                                                                                                            sWrapper: 'dataTables_wrapper form-inline dt-bootstrap',
+	                                                                                                            sFilterInput: 'form-control input-sm',
+	                                                                                                            sLengthSelect: 'form-control input-sm'
+        });
 
 
 /* Bootstrap paging button renderer */
-DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
-	var api     = new DataTable.Api( settings );
-	var classes = settings.oClasses;
-	var lang    = settings.oLanguage.oPaginate;
-	var btnDisplay, btnClass;
+        DataTable.ext.renderer.pageButton.bootstrap = function(settings, host, idx, buttons, page, pages) {
+	                                                                                                            var api = new DataTable.Api(settings);
+	                                                                                                            var classes = settings.oClasses;
+	                                                                                                            var lang = settings.oLanguage.oPaginate;
+	                                                                                                            var btnDisplay, btnClass;
 
-	var attach = function( container, buttons ) {
-		var i, ien, node, button;
-		var clickHandler = function ( e ) {
-			e.preventDefault();
-			if ( e.data.action !== 'ellipsis' ) {
-				api.page( e.data.action ).draw( false );
+	                                                                                                            var attach = function(container, buttons) {
+		                                        var i, ien, node, button;
+		                                        var clickHandler = function(e) {
+			                                        e.preventDefault();
+			                                        if (e.data.action !== 'ellipsis') {
+				                                        api.page(e.data.action).draw(false);
 			}
 		};
 
-		for ( i=0, ien=buttons.length ; i<ien ; i++ ) {
-			button = buttons[i];
+		                                        for (i = 0, ien = buttons.length; i < ien; i++) {
+			                                        button = buttons[i];
 
-			if ( $.isArray( button ) ) {
-				attach( container, button );
+			                                        if ($.isArray(button)) {
+				                                        attach(container, button);
 			}
-			else {
-				btnDisplay = '';
-				btnClass = '';
+			                                        else {
+				                                        btnDisplay = '';
+				                                        btnClass = '';
 
-				switch ( button ) {
+				                                        switch (button) {
 					case 'ellipsis':
-						btnDisplay = '&hellip;';
-						btnClass = 'disabled';
-						break;
+						                                        btnDisplay = '&hellip;';
+						                                        btnClass = 'disabled';
+						                                        break;
 
 					case 'first':
-						btnDisplay = lang.sFirst;
-						btnClass = button + (page > 0 ?
+						                                        btnDisplay = lang.sFirst;
+						                                        btnClass = button + (page > 0 ?
 							'' : ' disabled');
-						break;
+						                                        break;
 
 					case 'previous':
-						btnDisplay = lang.sPrevious;
-						btnClass = button + (page > 0 ?
+						                                        btnDisplay = lang.sPrevious;
+						                                        btnClass = button + (page > 0 ?
 							'' : ' disabled');
-						break;
+						                                        break;
 
 					case 'next':
-						btnDisplay = lang.sNext;
-						btnClass = button + (page < pages-1 ?
+						                                        btnDisplay = lang.sNext;
+						                                        btnClass = button + (page < pages - 1 ?
 							'' : ' disabled');
-						break;
+						                                        break;
 
 					case 'last':
-						btnDisplay = lang.sLast;
-						btnClass = button + (page < pages-1 ?
+						                                        btnDisplay = lang.sLast;
+						                                        btnClass = button + (page < pages - 1 ?
 							'' : ' disabled');
-						break;
+						                                        break;
 
 					default:
-						btnDisplay = button + 1;
-						btnClass = page === button ?
+						                                        btnDisplay = button + 1;
+						                                        btnClass = page === button ?
 							'active' : '';
-						break;
+						                                        break;
 				}
 
-				if ( btnDisplay ) {
-					node = $('<li>', {
-							'class': classes.sPageButton+' '+btnClass,
-							'aria-controls': settings.sTableId,
-							'tabindex': settings.iTabIndex,
-							'id': idx === 0 && typeof button === 'string' ?
-								settings.sTableId +'_'+ button :
+				                                        if (btnDisplay) {
+					                                        node = $('<li>', {
+							                                        'class': classes.sPageButton + ' ' + btnClass,
+							                                        'aria-controls': settings.sTableId,
+							                                        'tabindex': settings.iTabIndex,
+							                                        'id': idx === 0 && typeof button === 'string' ?
+								settings.sTableId + '_' + button :
 								null
-						} )
-						.append( $('<a>', {
-								'href': '#'
-							} )
-							.html( btnDisplay )
+						})
+						.append($('<a>', {
+								                                        'href': '#'
+							})
+							.html(btnDisplay)
 						)
-						.appendTo( container );
+						.appendTo(container);
 
-					settings.oApi._fnBindAction(
+					                                        settings.oApi._fnBindAction(
 						node, {action: button}, clickHandler
 					);
 				}
@@ -121,66 +120,63 @@ DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, bu
 		}
 	};
 
-	attach(
+	                                                                                                            attach(
 		$(host).empty().html('<ul class="pagination"/>').children('ul'),
 		buttons
 	);
-};
+        };
 
 
 /*
  * TableTools Bootstrap compatibility
  * Required TableTools 2.1+
  */
-if ( DataTable.TableTools ) {
+        if (DataTable.TableTools) {
 	// Set the classes that TableTools uses to something suitable for Bootstrap
-	$.extend( true, DataTable.TableTools.classes, {
-		"container": "DTTT btn-group",
-		"buttons": {
-			"normal": "btn btn-default",
-			"disabled": "disabled"
+	                                                                                                            $.extend(true, DataTable.TableTools.classes, {
+		                                    'container': 'DTTT btn-group',
+		                                    'buttons': {
+			                                    'normal': 'btn btn-default',
+			                                    'disabled': 'disabled'
 		},
-		"collection": {
-			"container": "DTTT_dropdown dropdown-menu",
-			"buttons": {
-				"normal": "",
-				"disabled": "disabled"
+		                                    'collection': {
+			                                    'container': 'DTTT_dropdown dropdown-menu',
+			                                    'buttons': {
+				                                    'normal': '',
+				                                    'disabled': 'disabled'
 			}
 		},
-		"print": {
-			"info": "DTTT_print_info modal"
+		                                    'print': {
+			                                    'info': 'DTTT_print_info modal'
 		},
-		"select": {
-			"row": "active"
+		                                    'select': {
+			                                    'row': 'active'
 		}
-	} );
+	});
 
 	// Have the collection use a bootstrap compatible drop down
-	$.extend( true, DataTable.TableTools.DEFAULTS.oTags, {
-		"collection": {
-			"container": "ul",
-			"button": "li",
-			"liner": "a"
+	                                                                                                            $.extend(true, DataTable.TableTools.DEFAULTS.oTags, {
+		                                    'collection': {
+			                                    'container': 'ul',
+			                                    'button': 'li',
+			                                    'liner': 'a'
 		}
-	} );
-}
-
-}; // /factory
+	});
+        }
+    }; // /factory
 
 
 // Define as an AMD module if possible
-if ( typeof define === 'function' && define.amd ) {
-	define( ['jquery', 'datatables'], factory );
-}
-else if ( typeof exports === 'object' ) {
+    if (typeof define === 'function' && define.amd) {
+	                                                                            define(['jquery', 'datatables'], factory);
+    }
+    else if (typeof exports === 'object') {
     // Node/CommonJS
-    factory( require('jquery'), require('datatables') );
-}
-else if ( jQuery ) {
+        factory(require('jquery'), require('datatables'));
+    }
+else if (jQuery) {
 	// Otherwise simply initialise as normal, stopping multiple evaluation
-	factory( jQuery, jQuery.fn.dataTable );
+	                                        factory(jQuery, jQuery.fn.dataTable);
 }
-
-
 })(window, document);
 
--- analytics_dashboard/static/vendor/domReady.js
+++ analytics_dashboard/static/vendor/domReady.js
@@ -3,17 +3,17 @@
  * Available via the MIT or new BSD license.
  * see: http://github.com/requirejs/domReady for details
  */
-/*jslint */
-/*global require: false, define: false, requirejs: false,
+/* jslint */
+/* global require: false, define: false, requirejs: false,
   window: false, clearInterval: false, document: false,
   self: false, setInterval: false */
 
 
-define(function () {
+define(function() {
     'use strict';
 
     var isTop, testDiv, scrollIntervalId,
-        isBrowser = typeof window !== "undefined" && window.document,
+        isBrowser = typeof window !== 'undefined' && window.document,
         isPageLoaded = !isBrowser,
         doc = isBrowser ? document : null,
         readyCalls = [];
@@ -29,7 +29,7 @@ define(function () {
         var callbacks = readyCalls;
 
         if (isPageLoaded) {
-            //Call the DOM ready callbacks
+            // Call the DOM ready callbacks
             if (callbacks.length) {
                 readyCalls = [];
                 runCallbacks(callbacks);
@@ -53,23 +53,23 @@ define(function () {
 
     if (isBrowser) {
         if (document.addEventListener) {
-            //Standards. Hooray! Assumption here that if standards based,
-            //it knows about DOMContentLoaded.
-            document.addEventListener("DOMContentLoaded", pageLoaded, false);
-            window.addEventListener("load", pageLoaded, false);
+            // Standards. Hooray! Assumption here that if standards based,
+            // it knows about DOMContentLoaded.
+            document.addEventListener('DOMContentLoaded', pageLoaded, false);
+            window.addEventListener('load', pageLoaded, false);
         } else if (window.attachEvent) {
-            window.attachEvent("onload", pageLoaded);
+            window.attachEvent('onload', pageLoaded);
 
             testDiv = document.createElement('div');
             try {
                 isTop = window.frameElement === null;
             } catch (e) {}
 
-            //DOMContentLoaded approximation that uses a doScroll, as found by
-            //Diego Perini: http://javascript.nwbox.com/IEContentLoaded/,
-            //but modified by other contributors, including jdalton
+            // DOMContentLoaded approximation that uses a doScroll, as found by
+            // Diego Perini: http://javascript.nwbox.com/IEContentLoaded/,
+            // but modified by other contributors, including jdalton
             if (testDiv.doScroll && isTop && window.external) {
-                scrollIntervalId = setInterval(function () {
+                scrollIntervalId = setInterval(function() {
                     try {
                         testDiv.doScroll();
                         pageLoaded();
@@ -78,18 +78,18 @@ define(function () {
             }
         }
 
-        //Check if document already complete, and if so, just trigger page load
-        //listeners. Latest webkit browsers also use "interactive", and
-        //will fire the onDOMContentLoaded before "interactive" but not after
-        //entering "interactive" or "complete". More details:
-        //http://dev.w3.org/html5/spec/the-end.html#the-end
-        //http://stackoverflow.com/questions/3665561/document-readystate-of-interactive-vs-ondomcontentloaded
-        //Hmm, this is more complicated on further use, see "firing too early"
-        //bug: https://github.com/requirejs/domReady/issues/1
-        //so removing the || document.readyState === "interactive" test.
-        //There is still a window.onload binding that should get fired if
-        //DOMContentLoaded is missed.
-        if (document.readyState === "complete") {
+        // Check if document already complete, and if so, just trigger page load
+        // listeners. Latest webkit browsers also use "interactive", and
+        // will fire the onDOMContentLoaded before "interactive" but not after
+        // entering "interactive" or "complete". More details:
+        // http://dev.w3.org/html5/spec/the-end.html#the-end
+        // http://stackoverflow.com/questions/3665561/document-readystate-of-interactive-vs-ondomcontentloaded
+        // Hmm, this is more complicated on further use, see "firing too early"
+        // bug: https://github.com/requirejs/domReady/issues/1
+        // so removing the || document.readyState === "interactive" test.
+        // There is still a window.onload binding that should get fired if
+        // DOMContentLoaded is missed.
+        if (document.readyState === 'complete') {
             pageLoaded();
         }
     }
@@ -115,7 +115,7 @@ define(function () {
     /**
      * Loader Plugin API method
      */
-    domReady.load = function (name, req, onLoad, config) {
+    domReady.load = function(name, req, onLoad, config) {
         if (config.isBuild) {
             onLoad(null);
         } else {
