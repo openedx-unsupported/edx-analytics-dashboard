a1b4fc074d1d31f219d2b00c1fe3b19790f8afbe
Anjali Pal <apal@edx.org>
2016-07-19 16:17:53 -0400

    Update enrollment graph to use new color choices

--- analytics_dashboard/static/js/enrollment-activity-main.js
+++ analytics_dashboard/static/js/enrollment-activity-main.js
@@ -11,7 +11,7 @@ require(['vendor/domReady!', 'load/init-page'], function (doc, page) {
             'views/stacked-trends-view'],
         function (DataTableView, StackedTrendsView) {
 
-            var colors = ['#4BB4FB', '#CA0061', '#CCCCCC'],
+            var colors = ['#4BB4FB', '#898C8F', '#009CD3', '#B72667', '#442255', '#1E8142'],
                 numericColumn = {
                     className: 'text-right',
                     type: 'number'
@@ -31,28 +31,28 @@ require(['vendor/domReady!', 'load/init-page'], function (doc, page) {
                         key: 'honor',
                         // Translators: this describe the learner's enrollment track (e.g. Honor certificate)
                         title: gettext('Honor'),
-                        color: colors[0]
+                        color: colors[1]
                     }),
                     _.defaults({}, numericColumn, {
                         key: 'audit',
                         title: gettext('Audit'),
-                        color: colors[0]
+                        color: colors[2]
                     }),
                     _.defaults({}, numericColumn, {
                         key: 'verified',
                         title: gettext('Verified'),
-                        color: colors[1]
+                        color: colors[3]
                     }),
                     _.defaults({}, numericColumn, {
                         key: 'professional',
                         title: gettext('Professional'),
-                        color: colors[2]
+                        color: colors[4]
                     }),
                     _.defaults({}, numericColumn, {
                         key: 'credit',
                         // Translators: this label indicates the learner has registered for academic credit
                         title: gettext('Verified with Credit'),
-                        color: colors[2]
+                        color: colors[5]
                     })
                 ],
                 trendSettings,
