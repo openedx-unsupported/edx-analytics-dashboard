77b85cf1a9a7a51425668ccc4d6a092a91e999aa
Tyler Hallada <thallada@edx.org>
2016-09-20 15:09:41 -0400

    Convert lowercase locales, update, & refactor.
    Commit messages for squashed debugging history:
    
    * Update cldr libs
    * Add test case for lowercasing language codes
    * Rename cldr language directories to fully lowercase
    * Upgrade font-awesome to 4.6.3
    * Use availableLocales & silence cldr in collectstatic
    * If window.language is undefined, default to "en"
    * Refactor globalize to allow tests to access fixLanguageCodes
    * More portable lowercasing method using tr
    * Use CLDR_DATA_PATH to be more DRY
    * Add mv_counter zero case message
    * Change directory to cldr-data main in script
    * Remove dir existence check. Doesn't work on mac
    * Bring back if condition. Compare strings.
    * Better solution: map lower to upper in js
    * Fix the tests

--- analytics_dashboard/static/js/utils/globalization.js
+++ analytics_dashboard/static/js/utils/globalization.js
@@ -1,4 +1,8 @@
-function fixLanguageCode(languageCode) {
+if (window.language === undefined) { // should only occur in test environments
+    window.language = 'en';
+}
+
+function fixLanguageCode(languageCode, lowerLocalesMapping) {
     'use strict';
     var fixedLanguageCode;
 
@@ -11,35 +15,39 @@ function fixLanguageCode(languageCode) {
     // CLDR uses zh for Simplified Chinese, while Django may use different strings.
     if (fixedLanguageCode === 'zh-cn' || fixedLanguageCode === 'zh-sg' ||
         fixedLanguageCode.indexOf('zh-hans') === 0) {
-        return 'zh';
+        fixedLanguageCode = 'zh';
     }
     // CLDR uses zh-hant for Traditional Chinese, while Django may use different strings.
     if (fixedLanguageCode === 'zh-tw' || fixedLanguageCode === 'zh-hk' ||
         fixedLanguageCode === 'zh-mo' || fixedLanguageCode.indexOf('zh-hant') === 0) {
-        return 'zh-hant';
+        fixedLanguageCode = 'zh-hant';
     }
 
     // There doesn't seem to be an onFailure event for the text! plugin. Make sure we only pass valid language codes
     // so the plugin does not attempt to load non-existent files.
-    if (['ar', 'ca', 'cs', 'da', 'de', 'el', 'en-001', 'en-au', 'en-ca', 'en-gb', 'en-hk', 'en-in', 'en', 'es', 'fi',
-        'fr', 'he', 'hi', 'hr', 'hu', 'it', 'ja', 'ko', 'nb', 'nl', 'pl', 'pt-pt', 'pt', 'ro', 'root', 'ru', 'sk', 'sl',
-        'sr', 'sv', 'th', 'tr', 'uk', 'vi', 'zh-hant', 'zh'].indexOf(fixedLanguageCode) > -1) {
-        return fixedLanguageCode;
+    if (fixedLanguageCode in lowerLocalesMapping) {
+        return lowerLocalesMapping[fixedLanguageCode];
     }
 
     return 'en';
 }
 
-window.language = fixLanguageCode(window.language);
-
 define([
     'globalize',
     'json!cldr-data/supplemental/likelySubtags.json',
     'json!cldr-data/supplemental/numberingSystems.json',
+    'json!cldr-data/availableLocales.json',  // eslint-disable-line import/no-unresolved
     'json!cldr-data/main/' + window.language + '/numbers.json',
     'globalize/number'
-], function(Globalize, likelySubtags, numberingSystems, numbers) {
+], function(Globalize, likelySubtags, numberingSystems, availableLocales, numbers) {
     'use strict';
+
+    var lowerLocalesMapping = {};
+    availableLocales.availableLocales.forEach(function(locale) {
+        lowerLocalesMapping[locale.toLowerCase()] = locale;
+    });
+
+    window.language = fixLanguageCode(window.language, lowerLocalesMapping);
 
     Globalize.load(likelySubtags);
     Globalize.load(numberingSystems);
