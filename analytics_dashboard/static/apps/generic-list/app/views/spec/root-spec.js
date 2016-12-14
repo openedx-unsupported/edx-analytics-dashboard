8b222e91089e0dd03fb0a7682634233d23350362
Tyler Hallada <thallada@edx.org>
2016-12-14 12:39:45 -0500

    root-spec.js

--- analytics_dashboard/static/apps/generic-list/app/views/spec/root-spec.js
+++ analytics_dashboard/static/apps/generic-list/app/views/spec/root-spec.js
@@ -3,13 +3,13 @@ define(function(require) {
 
     var Backbone = require('backbone'),
         Marionette = require('marionette'),
-        LearnersRootView = require('learners/app/views/root'),
+        RootView = require('generic-list/app/views/root'),
         PageModel = require('generic-list/common/models/page');
 
-    describe('LearnersRootView', function() {
+    describe('RootView', function() {
         beforeEach(function() {
             setFixtures('<div class=root-view-container></div>');
-            this.rootView = new LearnersRootView({
+            this.rootView = new RootView({
                 el: '.root-view-container',
                 pageModel: new PageModel({
                     title: 'Testing Title',
@@ -24,21 +24,21 @@ define(function(require) {
                     this.$el.html('example view');
                 }
             }))());
-            expect(this.rootView.$('.learners-main-region').html()).toContainText('example view');
+            expect(this.rootView.$('.main-region').html()).toContainText('example view');
         });
 
         it('renders a header title and date', function() {
-            expect(this.rootView.$('.learners-header-region').html()).toContainText('Testing Title');
-            expect(this.rootView.$('.learners-header-region').html()).not.toContainText('February 28, 2016');
+            expect(this.rootView.$('.header-region').html()).toContainText('Testing Title');
+            expect(this.rootView.$('.header-region').html()).not.toContainText('February 28, 2016');
         });
 
         it('renders and clears alerts', function() {
             var childView = new Marionette.View();
             this.rootView.showChildView('main', childView);
             childView.triggerMethod('appError', {title: 'This is the error copy'});
-            expect(this.rootView.$('.learners-alert-region')).toHaveText('This is the error copy');
+            expect(this.rootView.$('.alert-region')).toHaveText('This is the error copy');
             this.rootView.triggerMethod('clearError', 'This is the error copy');
-            expect(this.rootView.$('.learners-alert-region')).not.toHaveText('This is the error copy');
+            expect(this.rootView.$('.alert-region')).not.toHaveText('This is the error copy');
         });
     });
 });
