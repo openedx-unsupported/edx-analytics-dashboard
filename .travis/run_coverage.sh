609a8717897fbe3907f306b78ce2c9f0e5f52ed3
Dennis Jen <djen@edx.org>
2017-01-31 11:30:23 -0500

    Fixed run_coverage.sh

--- .travis/run_coverage.sh
+++ .travis/run_coverage.sh
@@ -2,8 +2,6 @@
 . /edx/app/insights/venvs/insights/bin/activate
 . /edx/app/insights/nodeenvs/insights/bin/activate
 
-cd /edx/app/insights/insights
-
 coverage xml
 
 bash ./scripts/build-stats-to-datadog.sh
