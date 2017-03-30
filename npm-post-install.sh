2cdc52f900fb251696ccbeb726cd2c9d16af3c67
Dennis Jen <djen@edx.org>
2017-03-30 10:11:41 -0400

    reference correct location for pattern library npm packages

--- npm-post-install.sh
+++ npm-post-install.sh
@@ -20,12 +20,13 @@ CLDR_DATA_PATH=${NPM_PATH}/cldr-data
 node ./node_modules/cldr-data-downloader/bin/download.js -i ${CLDR_DATA_PATH}/urls.json -o ${CLDR_DATA_PATH}
 
 # edX Pattern Library expects certain packages to be available to it
-PATTERN_LIBRARY_SASS_PATH="${NPM_PATH}/edx-pattern-library/pattern-library/sass"
+PATTERN_LIBRARY_PATH="${NPM_PATH}/edx-pattern-library"
+PATTERN_LIBRARY_SASS_PATH="${PATTERN_LIBRARY_PATH}/pattern-library/sass"
 PATTERN_LIBRARY_LIB_PATH="${PATTERN_LIBRARY_SASS_PATH}/global"
 PATTERN_LIBRARY_LIBS=('bi-app-sass' 'bourbon' 'breakpoint-sass' 'susy')
 for lib in "${PATTERN_LIBRARY_LIBS[@]}"
 do
-    cp -rf ${NPM_PATH}/$lib $PATTERN_LIBRARY_LIB_PATH/
+    cp -rf ${PATTERN_LIBRARY_PATH}/node_modules/$lib $PATTERN_LIBRARY_LIB_PATH/
 done
 
 # pre-compile the pattern library to improve sass compilation performance
