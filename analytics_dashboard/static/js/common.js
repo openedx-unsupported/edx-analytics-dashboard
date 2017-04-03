da97e953881e3771c8b425eec15f9492b6db67ba
Tyler Hallada <thallada@edx.org>
2017-04-03 14:31:44 -0400

    Add $ param to common function

--- analytics_dashboard/static/js/common.js
+++ analytics_dashboard/static/js/common.js
@@ -7,7 +7,7 @@ require([
     'vendor/domReady!',
     'load/init-page',
     'js/application-main'
-], function() {
+], function($) {
     'use strict';
 
     // Prevent XSS attack in jQuery 2.X: https://github.com/jquery/jquery/issues/2432#issuecomment-140038536
