ead2f324f8459aa37d22b7a6799f76c82239cc0c
Tyler Hallada <tyler@hallada.net>
2017-04-05 16:47:45 -0400

    Prevent XSS attack in jQuery 2.X with ajaxSetup (#658)
    * Prevent XSS attack in jQuery 2.X with ajaxSetup
    
    * Add $ param to common function

--- analytics_dashboard/static/js/common.js
+++ analytics_dashboard/static/js/common.js
@@ -7,6 +7,13 @@ require([
     'vendor/domReady!',
     'load/init-page',
     'js/application-main'
-], function() {
+], function($) {
     'use strict';
+
+    // Prevent XSS attack in jQuery 2.X: https://github.com/jquery/jquery/issues/2432#issuecomment-140038536
+    $.ajaxSetup({
+        contents: {
+            javascript: false
+        }
+    });
 });
