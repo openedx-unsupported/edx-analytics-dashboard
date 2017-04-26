fe9d485fbe287876808bdf92618001501cd47933
Tyler Hallada <thallada@edx.org>
2017-04-26 13:55:01 -0400

    Fix linting errors

--- analytics_dashboard/core/management/commands/delete_acceptance_test_soapbox_messages.py
+++ analytics_dashboard/core/management/commands/delete_acceptance_test_soapbox_messages.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from core.utils import delete_fake_soapbox_messages
 
+
 class Command(BaseCommand):
     """A command to delete the fake soapbox messages that the acceptance tests have completed checking."""
     help = 'Delete fake soapbox messages for testing.'
