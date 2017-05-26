7ab1480d10b3e928f5d86b22d651aa5189727bf8
Dennis Jen <djen@edx.org>
2017-05-26 14:07:00 -0400

    fixed linting error

--- analytics_dashboard/courses/presenters/course_summaries.py
+++ analytics_dashboard/courses/presenters/course_summaries.py
@@ -27,7 +27,7 @@ class CourseSummariesPresenter(BasePresenter):
         """
         all_summaries = cache.get(self.CACHE_KEY)
         if all_summaries is None:
-            exclude = ['programs'] # we make a separate call to the programs endpoint
+            exclude = ['programs']  # we make a separate call to the programs endpoint
             if not switch_is_active('enable_course_passing'):
                 exclude.append('passing_users')
             all_summaries = self.client.course_summaries().course_summaries(exclude=exclude)
