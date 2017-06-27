0c1d1a9a70d1628ce48733c4dbdd3a1c7fd5f02e
Tyler Hallada <thallada@edx.org>
2017-06-27 11:53:50 -0400

    Use CLOUDFRONT_DOMAIN var instead

--- analytics_dashboard/settings/production.py
+++ analytics_dashboard/settings/production.py
@@ -45,5 +45,5 @@ DOCUMENTATION_LOAD_ERROR_MESSAGE = 'This data may not be available for your cour
                                    '<a href="{error_documentation_link}" target="_blank">Read more</a>.'.format(error_documentation_link=DOCUMENTATION_LOAD_ERROR_URL)
 
 # Use Cloudfront CDN for assets
-if INSIGHTS_CLOUDFRONT:
-    STATIC_URL = 'https://' + INSIGHTS_CLOUDFRONT + '/'
+if CLOUDFRONT_DOMAIN:
+    STATIC_URL = 'https://' + CLOUDFRONT_DOMAIN + '/static/'
