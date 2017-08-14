d394ed2e83056135f879c57cd790fbd58351b72f
Tyler Hallada <thallada@edx.org>
2017-08-14 17:36:16 -0400

    Remove extra axe-core require in spec-runner.js

--- analytics_dashboard/static/js/test/specs/spec-runner.js
+++ analytics_dashboard/static/js/test/specs/spec-runner.js
@@ -1,5 +1,4 @@
 var context = require.context('../../../', true, /.+spec\.jsx?$/);
 require('../../load/init-tooltips.js');
-require('axe-core');
 context.keys().forEach(context);
 module.exports = context;
