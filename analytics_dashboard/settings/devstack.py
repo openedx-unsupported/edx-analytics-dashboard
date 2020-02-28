0cee483f73b54eac8e7210d174b83f33741e3374
Luis Moreno <luis.moreno@edunext.co>
2020-02-28 11:14:02 -0400

    Configure settings for devstack

--- analytics_dashboard/settings/devstack.py
+++ analytics_dashboard/settings/devstack.py
@@ -4,12 +4,3 @@ from __future__ import absolute_import
 from analytics_dashboard.settings.dev import *
 from analytics_dashboard.settings.yaml_config import *
 
-DATA_API_URL = os.getenv('DATA_API_URL', DATA_API_URL)
-DATA_API_AUTH_TOKEN = os.getenv('DATA_API_AUTH_TOKEN', DATA_API_AUTH_TOKEN)
-
-SOCIAL_AUTH_EDX_OAUTH2_ISSUER = "http://localhost:18000"
-SOCIAL_AUTH_EDX_OAUTH2_URL_ROOT = "http://edx.devstack.lms:18000"
-SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "http://localhost:18000"
-SOCIAL_AUTH_EDX_OAUTH2_LOGOUT_URL = "http://localhost:18000/logout"
-
-BACKEND_SERVICE_EDX_OAUTH2_PROVIDER_URL = "http://edx.devstack.lms:18000/oauth2"
