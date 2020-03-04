62b6e72150c6bf419863a53252759814baf7f131
Andrey Canon <andrey.canon@edunext.co>
2020-03-04 09:44:47 -0500

    Addressing or comments.

--- analytics_dashboard/wsgi.py
+++ analytics_dashboard/wsgi.py
@@ -19,11 +19,6 @@ import os
 from os.path import abspath, dirname
 from sys import path
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-
 SITE_ROOT = dirname(abspath(__file__))
 path.append(SITE_ROOT)
 
@@ -33,6 +28,10 @@ path.append(SITE_ROOT)
 # os.environ["DJANGO_SETTINGS_MODULE"] = "jajaja.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics_dashboard.settings.production")
 
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
