ca48f7631ce31ea0541ca8088015af3a701c8a28
Nizar Mahmoud <nizarmah@hotmail.com>
2021-03-10 20:54:30 +0300

    Adjusts the waffle flag description according to the latest recommendations

--- analytics_dashboard/courses/waffle.py
+++ analytics_dashboard/courses/waffle.py
@@ -9,10 +9,10 @@ from edx_toggles.toggles import NonNamespacedWaffleFlag
 # .. toggle_name: display_learner_analytics
 # .. toggle_implementation: WaffleFlag
 # .. toggle_default: False
-# .. toggle_description: Allows specified users to view Learner Analytics. Also displays the Learner Analytics tab and
-#   links to Learner Analytics on the Course Home page. Learner Analytics helps identify which learners are active and
-#   engaged and which aren't. It also provides an overview of their daily activity and their enrollment in a course.
+# .. toggle_description: Displays the Learner Analytics tab and links to Learner Analytics. Learner Analytics helps
+#   identify which learners are active and engaged and which aren't. It also provides an overview of their daily
+#   activity and their enrollment in a course. This is a rollout flag and is added and enabled by default for everyone.
 # .. toggle_warnings: Requires the `ModuleEngagementWorkflowTask` to be run to populate the charts.
-# .. toggle_use_cases: opt_in
+# .. toggle_use_cases: temporary
 DISPLAY_LEARNER_ANALYTICS = NonNamespacedWaffleFlag(
     'display_learner_analytics', __name__)
