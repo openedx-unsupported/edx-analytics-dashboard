e7838b87f82be00581d2adddc7476a4a3a629680
Zachary Hancock <zhancock@edx.org>
2021-09-03 10:15:34 -0400

    style: cleanup

--- a11y_tests/test_course_enrollment_demographics_axs.py
+++ a11y_tests/test_course_enrollment_demographics_axs.py
@@ -39,5 +39,4 @@ class CourseEnrollmentDemographicsAgeTests(CoursePageTestsMixin, WebAppTest):
         ).fulfill()
 
         # Check the page for accessibility errors
-        # import pdb; pdb.set_trace()
         report = self.page.a11y_audit.check_for_accessibility_errors()
