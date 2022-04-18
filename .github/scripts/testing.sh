3df1a762c1846a46e85ddb1b96abee927a092caf
Soban Javed <iamsobanjaved@gmail.com>
2022-04-18 17:55:14 +0500

    build: add support for node 16

--- .github/scripts/testing.sh
+++ .github/scripts/testing.sh
@@ -1,8 +1,20 @@
+if [[ $NODE == 16 ]]
+then
+  export NODE_VERSION=16.14.0
+else
+  export NODE_VERSION=12.11.1
+fi
+
 docker exec -t insights_testing bash -c "
     cd /edx/app/insights/edx_analytics_dashboard/ &&
     source /edx/app/insights/venvs/insights/bin/activate &&
     PATH=\$PATH:/edx/app/insights/nodeenvs/insights/bin:/snap/bin &&
     export TOXENV=django32 &&
     pip install -r requirements/github.txt &&
+    set -x &&
+    rm -rf /edx/app/insights/nodeenvs/insights-test-${NODE} &&
+    nodeenv --node=${NODE_VERSION} /edx/app/insights/nodeenvs/insights-test-${NODE} &&
+    source /edx/app/insights/nodeenvs/insights-test-${NODE}/bin/activate &&
+    node --version && npm --version
     make $TARGETS
 "
