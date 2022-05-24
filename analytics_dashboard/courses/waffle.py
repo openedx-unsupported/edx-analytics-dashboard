018d9ddf1a2e33e5a5bc01182937c5414d153987
Eugene Dyudyunov <evgen.dyudyunov@raccoongang.com>
2022-05-24 18:41:20 +0300

    refactor: rename toggle_warnings to toggle_warning (#1311)

--- analytics_dashboard/courses/waffle.py
+++ analytics_dashboard/courses/waffle.py
@@ -12,7 +12,7 @@ from edx_toggles.toggles import NonNamespacedWaffleFlag, SettingToggle
 # .. toggle_description: Displays the Learner Analytics tab and links to Learner Analytics. Learner Analytics helps
 #   identify which learners are active and engaged and which aren't. It also provides an overview of their daily
 #   activity and their enrollment in a course. This was a rollout flag and we recommend you enable this feature.
-# .. toggle_warnings: Requires the `ModuleEngagementWorkflowTask` to be run to populate the charts.
+# .. toggle_warning: Requires the `ModuleEngagementWorkflowTask` to be run to populate the charts.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2016-04-15
 # .. toggle_target_removal_date: 2016-07-15
