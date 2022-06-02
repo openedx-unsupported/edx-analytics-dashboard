ec2623697430c21e19afda1643a80d0a44ec2c21
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-06-02 14:12:41 +0500

    chore: replaced deprecated method. (#1316)

--- analytics_dashboard/courses/serializers.py
+++ analytics_dashboard/courses/serializers.py
@@ -1,5 +1,5 @@
 from django.core.serializers.json import DjangoJSONEncoder
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.functional import Promise
 
 
@@ -12,5 +12,5 @@ class LazyEncoder(DjangoJSONEncoder):
     # pylint: disable=method-hidden
     def default(self, obj):
         if isinstance(obj, Promise):
-            return force_text(obj)
+            return force_str(obj)
         return super().default(obj)
